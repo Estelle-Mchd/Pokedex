e7e9688aabc2eb812fb2a57ebd933f5032b870d7
Estelle Michard <michardestelleem@gmail.com>
2025-04-02 16:53:40 +0200

    Commit pour quête 3

--- src/components/PokemonCards.tsx
+++ src/components/PokemonCards.tsx
@@ -1,10 +1,24 @@
-const MyFigureComponent: React.FC = () => {
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+        imgSrc: "",
+    },
+];
+
+function PokemonCards() {
+    const pokemon = pokemonList[1];
+
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    )
+        <div>
+            <h2>{pokemon.name}</h2>
+            {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+        </div>
+    );
 }
 
-export default MyFigureComponent;
+export default PokemonCards;
