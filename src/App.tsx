6aaf4f6173118bc86b2acd81c81fb561baf49e43
Estelle Michard <michardestelleem@gmail.com>
2025-04-03 11:50:25 +0200

    Solution pour quête N°4 React Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import PokemonCards from './components/PokemonCards'
 
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
 function App() {
   return (
-    <div>
-      <PokemonCards />
-    </div>
+    <section>
+      <PokemonCards pokemon={pokemonList[0]} />
+    </section>
   );
 }
 
--- src/components/PokemonCards.tsx
+++ src/components/PokemonCards.tsx
@@ -1,16 +1,6 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
-function PokemonCards() {
-    const pokemon = pokemonList[1];
+function PokemonCards({ pokemon }) {
+
     return (
         <figure>
             {pokemon.imgSrc ? (
