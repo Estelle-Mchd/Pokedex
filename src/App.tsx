062d7fbae275a76cfc0a40f3264c3134d9886717
Estelle Michard <michardestelleem@gmail.com>
2025-04-03 17:05:33 +0200

    correction button

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       <nav>
         <div>
           {pokemonList.map((pokemon) => (
-            <button key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}</button>
+            <button type="button" key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}</button>
           ))}
         </div>
       </nav>
